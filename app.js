b6bd8291dbf3eb75c9093204f527952324906954
Andrea Zorzi <andrea@moku.io>
2026-02-09 17:34:32 +0100

    Revert "last chance"
    This reverts commit 94c5b3501b386f7562ffc99fb99a198431c2e2f4.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import app from './api/index.js';
 
 const port = process.env.PORT || 3000;
 
-// Removed to be complaint with Vercel deploying - just kept for development target
-if (process.env.NODE_ENV !== 'production') {
+// app.js run only on development mode, so the if is not needed anymore
+//if (process.env.NODE_ENV !== 'production') {
     app.listen(port, () => {
         console.log(`Sample app listening at http://localhost:${port}`);
     });
-}
+//}
