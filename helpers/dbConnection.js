0ce28ce256b7159e8e33c4e152b6b18465e5f6ff
Andrea Zorzi <andrea@moku.io>
2026-02-09 16:49:37 +0100

    fixed db connection

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -5,7 +5,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize(`${process.env.PGDATABASE_URL}`);
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Needed for Neon
+    }
+  },
+  logging: false // Optional: disable SQL logs in production
+});
 
 try {
   await sequelize.authenticate();
