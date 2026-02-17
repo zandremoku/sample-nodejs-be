98e70467f767347106dc93214f60ff1ff7309157
Andrea Zorzi <andrea@moku.io>
2026-02-17 17:54:15 +0100

    removed recommendation functions

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(userRoute);
-app.use(recommendationRoute);
+// app.use(recommendationRoute);
 app.use(travelExperienceRoute);
 
 // Only run migration once, not on every cold start
